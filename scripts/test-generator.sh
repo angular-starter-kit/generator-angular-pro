90a55aa74437be4bd36a926efdc60213f9baa857
Yohan Lasorsa <yohanlasorsa.thales@gmail.com>
2016-04-20 15:51:18 +0200

    Added test script with tests cases

---
+++ scripts/test-generator.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Simple testing script, that generates all kinds of projects and checks that
+# generated projects build and pass tests.
+
+set -e
+
+export CWD=`pwd`
+export SCRIPT_FOLDER=`dirname "${BASH_SOURCE[0]}"`
+export TEST_FOLDER=$CWD/sample-app
+export TEST_APP_NAME="Sample App"
+export TEST_CASES=$SCRIPT_FOLDER/test-cases/**/*.json
+
+function cleanup() {
+    cd $CWD
+    rm -rf $TEST_FOLDER
+}
+
+# Cleanup test folder in case of error
+trap cleanup ERR
+
+for file in $TEST_CASES
+do
+
+    echo
+    echo -------------------------------------------------------------
+    echo Testing generator with $file
+    echo -------------------------------------------------------------
+    echo
+
+    mkdir -p $TEST_FOLDER
+    cd $TEST_FOLDER
+
+    yo angular-pro --automate "$CWD/$file" $TEST_APP_NAME
+
+    gulp test
+    gulp clean && gulp protractor
+    gulp clean && gulp protractor:dist
+    gulp clean && gulp build
+
+    cleanup
+
+done
